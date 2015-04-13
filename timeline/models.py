d16e9c7f836c548fd55df4c14821b541464a902e
_Mark24_ <mark.zhangyoung@gmail.com>
2015-04-13 15:59:59 +0800

    增加了一些model删除了一个app

--- timeline/models.py
+++ timeline/models.py
@@ -1,4 +1,20 @@
 #! -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+#一条微博
+class Timeline(models.Model):
+	#avatar = models.ImageField()
+	content = models.TextField()#weibo内容
+	created_at = models.DateTimeField(auto_now_add=True)#创建时间
+	
+	class Meta(object):
+		db_table = 'timeline'
+
+#微博的评论
+class Comment(models.Model):
+	timeline = models.ForeignKey(Timeline,related_name="timeline_comments")
+	content = models.TextField()#评论内容
+	created_at = models.DateTimeField(auto_now_add=True)#创建时间
+
+	class Meta(object):
+		db_table = "timeline_comment"
